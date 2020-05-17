c660751d1e2e1dab6b7099227d93cca0a6fa6355
ipshitaRB <ipshitarburman@gmail.com>
2020-05-17 12:15:04 -0400

    Pass data between activities

--- app/src/main/java/com/droidrbi/gudetama/AboutActivity.kt
+++ app/src/main/java/com/droidrbi/gudetama/AboutActivity.kt
@@ -2,11 +2,20 @@ package com.droidrbi.gudetama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
+
+    lateinit var nameTextView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        nameTextView = findViewById(R.id.nameTextView)
+        val intent = getIntent()
+        val name  = intent.getStringExtra("name")
+        nameTextView.text = name
+
     }
 }
--- app/src/main/java/com/droidrbi/gudetama/MainActivity.kt
+++ app/src/main/java/com/droidrbi/gudetama/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.droidrbi.gudetama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var inputTextField:TextInputEditText
+    lateinit var aboutButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inputTextField = findViewById(R.id.nameTextField)
+        aboutButton = findViewById(R.id.aboutMeButton)
+
+        aboutButton.setOnClickListener { introduceGudetama() }
+    }
+
+    private fun introduceGudetama() {
+        val name: String = inputTextField.text.toString()
+        val intent = Intent(this@MainActivity, AboutActivity::class.java)
+        intent.putExtra("name", name)
+        startActivity(intent)
     }
 }
