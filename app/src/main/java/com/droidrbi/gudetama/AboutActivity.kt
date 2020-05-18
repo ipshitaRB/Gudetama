48fe93b2c7642eefa117a28a4908d89ac63a2ff3
ipshitaRB <ipshitarburman@gmail.com>
2020-05-17 20:49:16 -0400

    Add Toast message

--- app/src/main/java/com/droidrbi/gudetama/AboutActivity.kt
+++ app/src/main/java/com/droidrbi/gudetama/AboutActivity.kt
@@ -2,11 +2,14 @@ package com.droidrbi.gudetama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class AboutActivity : AppCompatActivity() {
 
     lateinit var nameTextView:TextView
+    lateinit var notYetButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +20,12 @@ class AboutActivity : AppCompatActivity() {
         val name  = intent.getStringExtra("name")
         nameTextView.text = name
 
+        notYetButton = findViewById(R.id.notYetButton)
+        notYetButton.setOnClickListener { sigh() }
+
+    }
+
+    private fun sigh() {
+        Toast.makeText(this, getString(R.string.disappointed),Toast.LENGTH_SHORT).show()
     }
 }
